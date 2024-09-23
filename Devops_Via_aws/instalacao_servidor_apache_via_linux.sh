989629cf1f2d4590e121a498761e6eddb8f9aac5
brunoavila <brunoap.avila@hotmail.com>
2024-09-23 18:59:02 -0300

    update

--- Devops_Via_aws/instalacao_servidor_apache_via_linux.sh
+++ Devops_Via_aws/instalacao_servidor_apache_via_linux.sh
@@ -1,9 +1,11 @@
+#!/bin/bash
+
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get install apache2 -y
 
-//Checando o servidor apache2
+#//Checando o servidor apache2
 systemctl status apache2
 
-//Para encontrar o ip do servidor
+#//Para encontrar o ip do servidor
 ip a 
